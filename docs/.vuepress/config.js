6ec3df62e62d2a7ad516189f453aeb46c11164d8
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 10:52:38 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,22 @@ import { defineUserConfig } from 'vuepress'
 
 export default {
   title:'RST Helps',
-  sidebar:'auto'
+  themeConfig: {
+    sidebar: [
+      {
+        title: '首页',   // 必要的
+        path: '/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
+      {
+        title: 'Group 2',
+        children: [ /* ... */ ],
+        initialOpenGroupIndex: -1 // 可选的, 默认值是 0
+      }
+    ]
+  }
 }
