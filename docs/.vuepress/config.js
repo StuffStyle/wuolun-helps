1190bb5d3007bd708186dad2c30d41cf1bf557b1
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 10:38:27 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: 'RST Helps',
   description: '回响项目组帮助中心，帮助您解决问题。',
-      // 不同子路径下的页面会使用不同的侧边栏
+    theme: defaultTheme({
     sidebar: [
       // SidebarItem
       {
@@ -14,4 +14,5 @@ export default defineUserConfig({
       // 字符串 - 页面文件路径
       '/bar/README.md',
     ],
+}),
 })
