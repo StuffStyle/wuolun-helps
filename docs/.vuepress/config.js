f38e84a8205ff342e2db9ca6c26fd024a8a23b99
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 11:22:44 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,4 @@
 import { defineUserConfig } from 'vuepress'
-const { sideBarTool } = require(path.join(__dirname, './index.js'))
-let unDirIncludes = ['node_modules', 'assets', 'public', '网络工程']
-let SuffixIncludes = ['md', 'html']
-let sidebar = sideBarTool.genSideBarGroup(rootPath, unDirIncludes, SuffixIncludes, {})
 export default {
   title:'RST Helps'
 }
