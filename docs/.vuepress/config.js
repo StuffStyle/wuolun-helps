ca300464ba32dfb3997008d7277143d0e3d13d21
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 11:15:41 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 import { defineUserConfig } from 'vuepress'
-const { sideBarTool } = require(path.join(__dirname, './utils/index.js'))
+const { sideBarTool } = require(path.join(__dirname, 'index.js'))
 let unDirIncludes = ['node_modules', 'assets', 'public', '网络工程']
 let SuffixIncludes = ['md', 'html']
 let sidebar = sideBarTool.genSideBarGroup(rootPath, unDirIncludes, SuffixIncludes, {})
