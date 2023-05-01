d1ab5374fa559c5bcc063823087d0f4ac82ceb00
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 10:43:41 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default {
   title:'RST Helps',
+themeConfig:{
     // 侧边栏数组
     // 所有页面会使用相同的侧边栏
     sidebar: [
@@ -23,4 +24,5 @@ export default {
       // 字符串 - 页面文件路径
       '/bar/README.md',
     ],
+},
 }
