c35ed73c4457067289046330f20b6b0a0b6609b8
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 10:46:12 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,27 +2,5 @@ import { defineUserConfig } from 'vuepress'
 
 export default {
   title:'RST Helps',
-themeConfig:{
-    // 侧边栏数组
-    // 所有页面会使用相同的侧边栏
-    sidebar: [
-      // SidebarItem
-      {
-        text: 'Foo',
-        link: '/foo/',
-        children: [
-          // SidebarItem
-          {
-            text: 'github',
-            link: 'https://github.com',
-            children: [],
-          },
-          // 字符串 - 页面文件路径
-          '/foo/bar.md',
-        ],
-      },
-      // 字符串 - 页面文件路径
-      '/bar/README.md',
-    ],
-},
+  sidebar:'auto'
 }
