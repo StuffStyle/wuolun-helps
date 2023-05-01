a23d03b84f1112e9f32c89fe010d02538056f4f3
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-01 10:22:55 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,6 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: 'RST Helps',
+  description: '回响项目组帮助中心，帮助您解决问题。',
 })
