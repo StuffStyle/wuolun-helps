d4a2d900aaa03a968f00e37881228d83f1f52193
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-02 09:48:56 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,25 @@ import { defineUserConfig } from 'vuepress'
 /* vuepress 的配置对象 */
 var config = {
     themeConfig: {
-        sidebar: 'auto'
+        sidebar: [
+      // SidebarItem
+      {
+        text: 'Foo',
+        link: '/foo/',
+        children: [
+          // SidebarItem
+          {
+            text: 'github',
+            link: 'https://github.com',
+            children: [],
+          },
+          // 字符串 - 页面文件路径
+          '/foo/bar.md',
+        ],
+      },
+      // 字符串 - 页面文件路径
+      '/bar/README.md',
+    ],
     },
 }
 
