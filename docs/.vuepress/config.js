5a500c87f6e5984bc2cd67bd34ed53542fac7682
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-02 09:35:31 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 import { defineUserConfig } from 'vuepress'
 export default {
   title:'RST Helps'
+  themeConfig: {
+    sidebar: {
+      '/vue/': [
+        'one',
+        'two'
+      ],
+      '/css/': [
+        'three',
+        'four'
+      ]
+    }
+  }
 }
