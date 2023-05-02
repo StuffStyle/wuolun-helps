781806a9858cd6c2ac0f84a3a0436bd889079644
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-02 09:46:42 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,9 @@
 import { defineUserConfig } from 'vuepress'
-const genSidebar = require('./utils/genSidebar.js');
-
-/* 生成侧边栏配置 */
-var sidebar = {
-    '/information': genSidebar.genDefaultSidebar('information', '常见问题（FAQ）', '知识库', true, 2),
-    /* fallback 侧边栏必须最后定义，否则会导致侧边栏无法使用 */
-    '/': ['README.md']
-}
 
 /* vuepress 的配置对象 */
 var config = {
     themeConfig: {
-        sidebar: sidebar,
+        sidebar: auto
     },
 }
 
