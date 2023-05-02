905e1e887ecfd58c2544e22c213f96eb806c2d6b
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-02 09:39:05 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,14 @@ import { defineUserConfig } from 'vuepress'
 export default {
   title:'RST Helps'
   themeConfig: {
-    sidebar: {
-      '/vue/': [
-        'one',
-        'two'
-      ],
-      '/css/': [
-        'three',
-        'four'
-      ],
-    },
-  },
+        sidebar: {
+          '/KnowledgeBase/FAQ/': [{
+              title: '常见问题（FAQ）',
+              children: [
+                {title:'AWTK',path:'1.AWTK'},
+                {title:'AWTK Designer',path:'2.AWTK Designer’}
+              ]
+            }]
+         }
+     }
 }
