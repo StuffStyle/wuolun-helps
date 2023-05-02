b831dff0ec4b1ff467a81117269e852ac15c527b
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-02 09:43:23 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,22 @@
 import { defineUserConfig } from 'vuepress'
+const genSidebar = require('./utils/genSidebar.js');
+...
+/* 生成侧边栏配置 */
+var sidebar = {
+    '/information': genSidebar.genDefaultSidebar('information', '常见问题（FAQ）', '知识库', true, 2),
+    /* fallback 侧边栏必须最后定义，否则会导致侧边栏无法使用 */
+    '/': ['']
+}
+
+/* vuepress 的配置对象 */
+var config = {
+    ...
+    themeConfig: {
+        sidebar: sidebar,
+    },
+}
+
+
 export default {
   title:'RST Helps'
-  themeConfig: {
-        sidebar: {
-          '/KnowledgeBase/FAQ/': [{
-              title: '常见问题（FAQ）',
-              children: [
-                {title:'AWTK',path:'1.AWTK'},
-                {title:'AWTK Designer',path:'2.AWTK Designer’}
-              ]
-            }]
-         }
-     }
 }
