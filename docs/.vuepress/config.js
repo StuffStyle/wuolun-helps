f98c634478008796f769e93114f66541349b5b19
OverRay <108870114+StuffStyle@users.noreply.github.com>
2023-05-02 10:24:00 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,22 @@
 //引入默认主题
 import { defaultTheme } from 'vuepress'
 export default {
+        title: 'RST Helps',
 	theme: defaultTheme({
 	// favicon.ico网站收藏夹图标，图片本地地址在public文件夹内，图片地址也可选填网络地址
 	head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
 	logo: 'favicon.png', //文件在public文件夹内
 	repo: 'StuffStyle',  //默认识别为GitHub仓库
-        sidebar: 'auto',
 	 navbar: [
       // NavbarItem
       {
-        text: 'Foo',
-        link: '/foo/',
+        text: '首页',
+        link: '/',
       },
       // NavbarGroup
       {
-        text: 'Group',
-        children: ['/group/foo.md', '/group/bar.md'],
+        text: '分类',
+        children: ['README.md', '/group/bar.md'],
       },
       // 字符串 - 页面文件路径
       '/bar/README.md',
